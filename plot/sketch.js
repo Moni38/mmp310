a60efa42ddee3b43d7fd229081f08d4ace72d91f
Owen Roberts <oroberts@bmcc.cuny.edu>
2019-02-13 18:15:15 -0500

    updated plot functions

--- plot/sketch.js
+++ plot/sketch.js
@@ -5,18 +5,13 @@
 */
 
 // global variables
-var story = "Once upon a time, there was an orange and a banana.";
-var storyX;
-var storyY;
-
 var chapter = "morning"; // day, night
+var whichFruit;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	textSize(40);
 	textAlign(CENTER, CENTER);
-	storyX = width / 2;
-	storyY = height - 50;
+	textFont('Hanalei Fill');
 }
 
 function draw() {
@@ -24,23 +19,45 @@ function draw() {
 	
 	if (chapter == "morning") {
 		orange(100, 200);  // first character
-		banana(200, 100); // second character 
+		banana(200, 100); // second character
+		narrative("Once upon a time, there was an orange and a banana.", "Click anywhere to continue.");
 	} else if (chapter == "day") {
 		orange(300, 150);  
 		banana(400, 75);
+		narrative("Then the user ate one of them.", "Click on one of the characters to eat it.");
 	} else if (chapter == "night") {
-		orange(600, 100);  
-		banana(700, 50);
+		if (whichFruit == "banana") {
+			orange(600, 100); 
+			narrative("The orange was alone forever.", "Click anywhere to start over.");
+		}
+		if (whichFruit == "orange") {
+			banana(700, 50);
+			narrative("The banana was alone forever.", "Click anywhere to start over.");
+		}
 	}
-	
-	stroke(255);
-	text(story, storyX, storyY);
 }
 
 function mouseClicked() {
+	// change chapter
 	if (chapter == "morning") chapter = "day";
-	else if (chapter == "day") chapter = "night";
+//	else if (chapter == "day") chapter = "night";
 	else if (chapter == "night") chapter = "morning";
+	
+	// detect which character is clicked
+	else if (chapter == "day") {
+		// clicked the orange
+		var orangeDist = dist(mouseX, mouseY, 300, 200);
+		if (orangeDist < 150 / 2) {
+			whichFruit = "orange";
+			chapter = "night";
+		}
+		// clicked banana
+		var bananaDist = dist(mouseX, mouseY, 400, 100);
+		if (bananaDist < 75 / 2) {
+			whichFruit = "banana";
+			chapter = "night";
+		}
+	}
 }
 
 function orange(x, s) {
@@ -52,6 +69,8 @@ function orange(x, s) {
 	stroke(0);
 	ellipse(x + eyeSize * 3, 200 - eyeSize, eyeSize);  // left eye
 	ellipse(x, 200 - eyeSize, eyeSize); 		// right eye
+	
+//	rect(100, 220, s / 2, s / 4); // mouth
 }
 
 function banana(x, s) {
@@ -65,7 +84,15 @@ function banana(x, s) {
 	arc(x, 100, s * 3/4, s, -HALF_PI, HALF_PI); // banana contour
 }
 
-
+function narrative(story, instructions) {
+	// narrative
+	stroke(255);
+	textSize(30);
+	text(story, width / 2, height - 100);
+	// instructions
+	textSize(20);
+	text(instructions, width / 2, height - 50);
+}
 
 
 
